e9aa99c253fcaf838730f790968a45a878f16b42
laurasr <lauransotomayor5@gmail.com>
2024-07-03 17:12:31 +1000

    base fair_go_phd

--- Home.py
+++ Home.py
@@ -5,7 +5,7 @@ st.set_page_config(layout="wide")
 
 # Customize the sidebar
 markdown = """
-A Streamlit map template
+Fair Go for PhDs
 <https://github.com/opengeos/streamlit-map-template>
 """
 
@@ -15,7 +15,7 @@ logo = "https://i.imgur.com/UbOXYAU.png"
 st.sidebar.image(logo)
 
 # Customize page title
-st.title("Streamlit for Geospatial Applications")
+st.title("Fair Go for PhDs")
 
 st.markdown(
     """
