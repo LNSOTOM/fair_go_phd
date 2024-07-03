e3e3f8c081e393307af00e13ebc83947be32650d
laurasr <lauransotomayor5@gmail.com>
2024-07-03 17:33:49 +1000

    base

--- Home.py
+++ Home.py
@@ -11,7 +11,7 @@ Fair Go for PhDs
 
 st.sidebar.title("About")
 st.sidebar.info(markdown)
-logo = r"C:\Users\laurasr\code\fair_go_phd\assets\instagram _1.png"
+logo = "C:/Users/laurasr/code/fair_go_phd/assets/instagram _1.png"
 st.sidebar.image(logo)
 
 # Customize page title
