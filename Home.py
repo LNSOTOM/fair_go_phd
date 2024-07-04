05fd384a7bd2cb1a0ee627ca4ad7d668bb8d4ab3
laurasr <lauransotomayor5@gmail.com>
2024-07-04 10:31:25 +1000

    github

--- Home.py
+++ Home.py
@@ -26,7 +26,7 @@ st.markdown(
 st.header("Instructions")
 
 markdown = """
-1. For the [GitHub repository](https://github.com/opengeos/streamlit-map-template) or [use it as a template](https://github.com/opengeos/streamlit-map-template/generate) for your own project.
+1. For the [GitHub repository](https://github.com/LNSOTOM/fair_go_phd/tree/main) or [use it as a template](https://github.com/opengeos/streamlit-map-template/generate) for your own project.
 2. Customize the sidebar by changing the sidebar text and logo in each Python files.
 3. Find your favorite emoji from https://emojipedia.org.
 4. Add a new app to the `pages/` directory with an emoji in the file name, e.g., `1_🚀_Chart.py`.
