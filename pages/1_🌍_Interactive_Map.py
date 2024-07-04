185a280f270122b601421b45c1dea6cca3149404
laurasr <lauransotomayor5@gmail.com>
2024-07-04 11:04:11 +1000

    basemap

--- pages/1_🌍_Interactive_Map.py
+++ pages/1_🌍_Interactive_Map.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import leafmap.foliumap as leafmap
 
+st.set_page_config(layout="wide")
 
 # Customize the sidebar
 markdown = """
@@ -13,27 +14,42 @@ st.sidebar.info(markdown)
 logo = "assets/instagram _1.png"
 st.sidebar.image(logo)
 
-
 st.title("Interactive Map")
 
-col1, col2 = st.columns([4, 1])
+# Add custom CSS to position the select box
+st.markdown("""
+    <style>
+    .selectbox-container {
+        position: absolute;
+        top: 10px;
+        right: 10px;
+        z-index: 1000;
+        background-color: white;
+        padding: 10px;
+        border-radius: 5px;
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    }
+    </style>
+""", unsafe_allow_html=True)
+
+# Create a container for the select box
+st.markdown('<div class="selectbox-container">', unsafe_allow_html=True)
+
 options = list(leafmap.basemaps.keys())
 index = options.index("SATELLITE")
 
-with col2:
+basemap = st.selectbox("Select a basemap:", options, index)
 
-    basemap = st.selectbox("Select a basemap:", options, index)
+st.markdown('</div>', unsafe_allow_html=True)
 
+m = leafmap.Map(
+    center=[-25.2744, 133.7751],
+    zoom=4,
+    locate_control=True,
+    latlon_control=True,
+    draw_export=True,
+    # minimap_control=True
+)
+m.add_basemap(basemap)
+m.to_streamlit(height=700)
 
-with col1:
-
-    m = leafmap.Map(
-        center=[-25.2744, 133.7751],
-        zoom =4, 
-        locate_control=True, 
-        latlon_control=True, 
-        draw_export=True, 
-        # minimap_control=True
-    )
-    m.add_basemap(basemap)
-    m.to_streamlit(height=700)
