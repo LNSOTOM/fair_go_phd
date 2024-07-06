f66386f8f4bd9397f7d666475a4315a440ef975e
Ilaria Stollberg <85492378+iles-stell@users.noreply.github.com>
2024-07-06 20:08:47 +1000

    Update 2_Cost_of_Living_and_PhDs.py

--- pages/2_Cost_of_Living_and_PhDs.py
+++ pages/2_Cost_of_Living_and_PhDs.py
@@ -8,12 +8,14 @@ st.title("How has Cost of Living affected PhD students?")
 # Read the CSV file
 df = pd.read_csv("assets/hdr_rates.csv")
 
-
-# Plotting
+# Plotting up figure
 fig = plt.figure(figsize=(10, 6))  # Adjust the figure size 
-plt.plot(df['Year'], df['weekly_full-time_base_rate_AUD'], marker='o', linestyle='-')
+plt.plot(df['Year'], df['weekly_full-time_base_rate_AUD'], marker='o', linestyle='-', label='Minimum HDR stipend')  # Plot minimum HDR stipend
+plt.plot(df['Year'], df['weekly_poverty_thresh_AUD'], marker='s', linestyle='--', color='orange', label='Poverty line')  # Plot poverty line
+
 plt.xlabel('Year')  # x-axis label
-plt.ylabel('Minimum HDR stipend rate (AUD)')  # y-axis label
+plt.ylabel('Weekly HDR stipend rate (AUD)')  # y-axis label
+plt.title('Minimum HDR Stipend vs Poverty Line')  # Plot title
 
 # Display plot in Streamlit
 st.pyplot(fig)
