ea0e03f218036681ec545ce7fb66e3d90f63ee5a
Ilaria Stollberg <85492378+iles-stell@users.noreply.github.com>
2024-07-06 19:34:04 +1000

    Update 2_Cost_of_Living_and_PhDs.py

--- pages/2_Cost_of_Living_and_PhDs.py
+++ pages/2_Cost_of_Living_and_PhDs.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
+import mpld3
 
 # Title of the app
 st.title("How has Cost of Living affected PhD students?")
@@ -8,11 +9,14 @@ st.title("How has Cost of Living affected PhD students?")
 # Read the CSV file
 df = pd.read_csv("assets/AllGroupsCPI.csv")
 
+
 # Plotting
-plt.figure(figsize=(10, 6))  # Adjust the figure size 
+fig = plt.figure(figsize=(10, 6))  # Adjust the figure size 
 plt.plot(df['col_1'], df['col_3'], marker='o', linestyle='-')
 plt.xlabel('Time')  # x-axis label
 plt.ylabel('CPI')  # y-axis label
 
 # Display plot in Streamlit
-st.pyplot()
+#st.pyplot()
+fig_html = mpld3.fig_to_html(fig)
+components.html(fig_html, height=600)
