7ee29946b6e0b133baa8e620d28536f2ba6d0ce1
Ilaria Stollberg <85492378+iles-stell@users.noreply.github.com>
2024-07-06 20:05:29 +1000

    Update 2_Cost_of_Living_and_PhDs.py

--- pages/2_Cost_of_Living_and_PhDs.py
+++ pages/2_Cost_of_Living_and_PhDs.py
@@ -6,14 +6,14 @@ import matplotlib.pyplot as plt
 st.title("How has Cost of Living affected PhD students?")
 
 # Read the CSV file
-df = pd.read_csv("assets/AllGroupsCPI.csv")
+df = pd.read_csv("assets/hdr_rates.csv")
 
 
 # Plotting
 fig = plt.figure(figsize=(10, 6))  # Adjust the figure size 
-plt.plot(df['col_1'], df['col_3'], marker='o', linestyle='-')
-plt.xlabel('Time')  # x-axis label
-plt.ylabel('CPI')  # y-axis label
+plt.plot(df['Year'], df['weekly_full-time_base_rate_AUD'], marker='o', linestyle='-')
+plt.xlabel('Year')  # x-axis label
+plt.ylabel('Minimum HDR stipend rate (AUD)')  # y-axis label
 
 # Display plot in Streamlit
 st.pyplot(fig)
