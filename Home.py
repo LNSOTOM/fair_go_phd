f83d7ca56929067545d283418bde384e45bbb533
Ilaria Stollberg <85492378+iles-stell@users.noreply.github.com>
2024-07-06 15:24:22 +1000

    Update Home.py

--- Home.py
+++ Home.py
@@ -24,28 +24,28 @@ st.header("Who are PhD students?")
 
 st.markdown(
 """
-1. A PhD is the highest obtainable degree in a given discipline, and it results from a high-quality contribution to the field.\\
-2. Approximately ~50,000 PhD students are active within Australia; the majority work full-time on their project, which has a duration of 3.5 years\\
-3. Once they graduate, PhD students may go on to stay within academia, or work in industry.
+1. A PhD is the highest obtainable degree in a given discipline, and it results from a high-quality contribution to the field.\
+2. Approximately ~50,000 PhD students are active within Australia; the majority work full-time on their project, which has a duration of 3.5 years\
+3. Once they graduate, PhD students may go on to stay within academia, or work in industry.\
 """
 )
 
 st.header("What do PhD students do?")
 
 markdown = """
-    1. PhD students work on ground breaking, state of the art, frontline research under the supervision of a team of experts.\\
-    2. PhD students work 40 hours a week and receive allocated time off (similarly to staff members).\\
+    1. PhD students work on ground breaking, state of the art, frontline research under the supervision of a team of experts.\
+    2. PhD students work 40 hours a week and receive allocated time off (similarly to staff members).\
 """
 
 st.markdown(markdown)
 
-st.header("Where are we")
+#st.header("Where are we")
 
-m = leafmap.Map(center=[-25.2744, 133.7751], zoom=4, minimap_control=True)
+#m = leafmap.Map(center=[-25.2744, 133.7751], zoom=4, minimap_control=True)
 # m.add_basemap("OpenTopoMap")
-m.add_tile_layer(
-    url="https://mt1.google.com/vt/lyrs=y&x={x}&y={y}&z={z}",
-    name="Google Satellite",
-    attribution="Google",
-)
-m.to_streamlit(height=500)
+#m.add_tile_layer(
+ #   url="https://mt1.google.com/vt/lyrs=y&x={x}&y={y}&z={z}",
+  #  name="Google Satellite",
+   # attribution="Google",
+#)
+#m.to_streamlit(height=500)
