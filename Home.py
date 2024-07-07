84f4828c9e1621221f8e9fa9629cb641f14b9f33
Ilaria Stollberg <85492378+iles-stell@users.noreply.github.com>
2024-07-07 12:22:39 +1000

    Update Home.py

--- Home.py
+++ Home.py
@@ -26,6 +26,7 @@ st.markdown(
 """
 Go Fair for PhDs is a campaign led by PhD students at the University of Tasmania (UTAS). Our goal is to ensure the just compensation of PhD students nationwide: we want universities to raise the minimum PhD stipend to match the national minimum wage, so that PhD students can live with dignity and focus on their research.
 """
+)
 
 st.header("Who are PhD students?")
 
