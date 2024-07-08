3c8a96f17eb571cdc46f2cd217b76b126815b8ad
Ilaria Stollberg <85492378+iles-stell@users.noreply.github.com>
2024-07-08 12:33:52 +1000

    Update Home.py

--- Home.py
+++ Home.py
@@ -20,39 +20,39 @@ st.sidebar.image(logo)
 infog1 = "assets/1.png"
 st.image(infog1, use_column_width=True)
 
-st.header("Our goal: fair wages for Australia's PhD students.")
+st.header("Our goal: fair wages for Australia's PhDs.")
 
 st.markdown(
 """
-Go Fair for PhDs is a campaign led by PhD students at the University of Tasmania (UTAS). Our goal is to ensure the just compensation of PhD students nationwide: we want universities to raise the minimum PhD stipend to match the national minimum wage, so that PhD students can live with dignity, afford housing and essentials, and focus on their research.
+'Go Fair for PhDs' is a campaign led by PhDs, for PhDs at the University of Tasmania (UTAS). Our goal is to ensure the just compensation of PhDs nationwide: we want the Australian government to raise the minimum stipend to match the national minimum wage, so that PhDs can live with dignity, afford housing and essentials, and focus on their research.
 """
 )
 
-st.subheader("Who are PhD students?")
+st.subheader("Who are PhDs?")
 
 st.markdown(
 """
 A PhD is the highest obtainable degree in a given discipline, and it results from a high-quality contribution to the field.  
-Approximately ~50,000 PhD students are active within Australia; the majority work full-time on their project, which has a duration of 3.5 years.  
-Once they graduate, PhD students may go on to stay within academia, or work in industry.  
+Approximately ~50,000 PhDs are active within Australia; the majority work full-time on their project, which has a duration of 3.5 years.  
+Once they graduate, PhDs may go on to stay within academia, or work in industry.  
 """
 )
 
-st.subheader("What do PhD students do?")
+st.subheader("What do PhDs do?")
 
 st.markdown(
 """
-PhD students work on ground breaking, state of the art, frontline research with the guidance of a team of experts - this dynamic is similar to a graduate program, where a new employee is guided by senior figures to become fully qualified.  
-PhD students work at least 40 hours a week and receive allocated time off (similarly to fully salaried staff members).  
-PhD students have a very different workload and schedule compared to undergraduate or master students: they are expected to invest most of their working hours into their independent project, and do not attend classes or have time off during traditional school breaks.
+PhDs work on ground breaking, state of the art, frontline research with the guidance of a team of experts - this dynamic is similar to a graduate program, where a new employee is guided by senior figures to become fully qualified.  
+PhDs work at least 38 hours a week (and often more) and receive allocated time off similarly to fully salaried staff members.  
+PhDs have a very different workload and schedule compared to undergraduate or master students: they are expected to invest most of their working hours into their independent project, and do not attend classes or have time off during traditional school breaks.
 """
 )
 
-st.header("PhD students are a core driving force in research and innovation.")
+st.header("PhDs are a core driving force in research and innovation.")
 
 st.markdown(
 """
-PhD students make up over 50% of human resources dedicated to research in Australian universities. Their effort is key in keeping Australian research relevant and competitive.
+PhDs make up over 50% of human resources dedicated to research in Australian universities. Their effort is key in keeping Australian research relevant and competitive.
 """
 )
 
@@ -63,10 +63,10 @@ st.subheader("Why is this happening?")
 
 st.markdown(
 """
-PhD researchers receive a higher degree by research (HDR) stipend under the research training program (RTP) by the national government. As of 2024, the minimum full-time stipend is AUD 32,192.  
-The 2024 PhD Stipend survey found that, even with top ups, most PhD researchers earn significantly less than the National Minimum Wage (AUD 47,627). Additionally, part-time PhD stipends are taxed.  
-Not only do PhD students not receive the equivalent of minimum wages, they also miss out on superannuation for several years.  
-Universities can (and do) limit the amount of hours PhD researchers are allowed to take on outside work during business hours, reducing opportunities for extra income.
+PhDs receive a Higher Degree by Research (HDR) stipend under the Research Training Program (RTP) by the national government. As of 2024, the minimum full-time stipend is AUD 32,192.  
+The 2024 PhD Stipend survey found that, even with top ups, most PhDs earn significantly less than the National Minimum Wage (AUD 47,627). Additionally, part-time PhD stipends are taxed.  
+Not only do PhDs not receive the equivalent of minimum wages, they also miss out on superannuation for several years.  
+Universities can (and do) limit the amount of hours PhDs are allowed to take on outside work during business hours, reducing opportunities for extra income.
 """
 )
 
@@ -78,7 +78,7 @@ st.markdown(
 There are different ways to help:
 1. Sign the e-petition submitted to the House of Representatives [here](https://www.aph.gov.au/e-petitions/petition/EN6358)  
 2. Share our campaign on social media and within your networks  
-3. Join us to help with our activities. We need a nation-wide network of PhD Researchers to make our collective voice heard!​  
+3. Join us to help with our activities. We need a nation-wide network of PhDs, researchers, and senior staff to make our collective voice heard!​  
 4. Share your own PhD experience with local media or [with us](https://fair-go-phd.streamlit.app/Get_in_touch)
 """
 )
