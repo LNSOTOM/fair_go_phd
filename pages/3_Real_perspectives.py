2fe5ecb1170172a78df1d075aec9659284877b48
Ilaria Stollberg <85492378+iles-stell@users.noreply.github.com>
2024-07-08 12:47:17 +1000

    Update 3_Real_perspectives.py

--- pages/3_Real_perspectives.py
+++ pages/3_Real_perspectives.py
@@ -14,10 +14,11 @@ logo = "assets/instagram _1.png"
 st.sidebar.image(logo)
 
 # Title of the app
-st.title("Groceries, heating, or a doctor's visit?")
+st.title("Groceries, heating, or a doctor's appointment?")
 
 st.markdown(
 """
-"I work over 40 hours per week on my PhD. The only way I can afford to do this is because I have a partner that has a well paying full-time job. I genuinely don't know how I would be able to do my research without him. Even so, we have to budget pretty carefully.  All you need is to have one unexpected dentist visit come up, and your bank account is empty. We are well into our 30s and can't even think about having kids, there is no way we could afford that right now. And just to re-iterate, I am very lucky by comparison. It's heart-breaking witnessing colleagues debate whether they should buy groceries or heat their homes. Many of us had careers before we started this. I had a competitive salary above Minimum Wage." - B, PhD on Fisheries Management
+"I work over 40 hours per week on my PhD. The only way I can afford to do this is because I have a partner that has a well paying full-time job. I genuinely don't know how I would be able to do my research without him. Even so, we have to budget pretty carefully.  All you need is to have one unexpected dentist visit come up, and your bank account is empty. We are well into our 30s and can't even think about having kids, there is no way we could afford that right now. And just to re-iterate, I am very lucky by comparison. It's heart-breaking witnessing colleagues debate whether they should buy groceries or heat their homes. Many of us had careers before we started this. I had a competitive salary above Minimum Wage."   
+- B, PhD on Fisheries Management
 """
 )
